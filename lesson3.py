6de9d9ea5b934e252ee2d6fce9ee4c168687e5a5
Alexandr Syromyatnikov <ambazur@gmail.com>
2023-03-30 22:50:24 +0600

    lesson3 more change

--- lesson3.py
+++ lesson3.py
@@ -2,3 +2,8 @@ print('Hello World')
 name = input('What is your name?: ')
 
 print('Hi,', name, '!')
+age = int(input('How old are you? '))
+if age > 20:
+    print('Old bitch!')
+else:
+    print('Little bitch!')
